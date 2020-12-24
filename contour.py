b12c90725f956897be808ca978024b04c14779d0
sachitha2 <shpsachitha@gmail.com>
2020-12-24 19:56:12 +0530

    contours

---
+++ contour.py
@@ -0,0 +1,41 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('photos/cat.jpg')
+
+cv.imshow("Cat",img)
+
+
+blank = np.zeros(img.shape,dtype='uint8')
+cv.imshow('blank',blank)
+
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+cv.imshow("Gray",gray)
+
+blur = cv.GaussianBlur(gray,(5,5),cv.BORDER_DEFAULT)
+
+cv.imshow('Blur',blur)
+
+canny = cv.Canny(blur,125,175)
+
+
+cv.imshow("Canny",canny)
+
+
+# Binary format converting
+ret, thresh  = cv.threshold(gray,125,255,cv.THRESH_BINARY)
+
+
+cv.imshow('thresh',thresh)
+
+
+contours , hierarchies = cv.findContours(canny,cv.RETR_LIST,cv.CHAIN_APPROX_NONE)
+
+print(len(contours))
+
+cv.drawContours(blank,contours, -1,(0,0,255),1)
+
+cv.imshow('new blank',blank)
+
+cv.waitKey(0)
