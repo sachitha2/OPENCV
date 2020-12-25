b5307ece8945b325b8b2a96b5b9b5ace432e3558
sachitha2 <shpsachitha@gmail.com>
2020-12-25 14:02:54 +0530

    wip color spaces

--- colorSpaces.py
+++ colorSpaces.py
@@ -1,8 +1,31 @@
 import cv2 as cv
+import matplotlib.pyplot as plt
 
 img = cv.imread('photos/cat.jpg')
 
 cv.imshow('cat',img)
 
+# plt.imshow(img)
+# plt.show()
+
+
+# BGR To Grayscale
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+cv.imshow('gray',gray)
+
+# BGR to HSV 
+hsv = cv.cvtColor(img,cv.COLOR_BGR2HSV)
+cv.imshow('hsv',hsv)
+
+# BGR to LAB L*A*B
+lab = cv.cvtColor(img,cv.COLOR_BGR2LAB)
+cv.imshow('LAB',lab)
+
+# BGR to RGB
+rgb = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+cv.imshow('RGB',rgb)
+
+plt.imshow(rgb)
+plt.show()
 
 cv.waitKey(0)
